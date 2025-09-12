fe7e77601841531592c282a7147e5d323ed86422
anubhavbhardwaj2004-sketch <anubhavbhardwaj2004@gmail.com>
2025-09-12 20:17:06 +0530

    link

--- index.js
+++ index.js
@@ -6,9 +6,20 @@ function toggle() {
     menu.style.transform = 'scale(1)';
     menu.style.visibility = 'visible';
     menu.style.opacity = '1';
+    menu.style.backgroundColor = 'rgba(0, 0, 0, 0.8) opacity(-10)';
   } else {
     menu.style.transform = 'scale(0)';
     menu.style.visibility = 'hidden';
     menu.style.opacity = '0';
   }
 }
+const banner = document.querySelector('.banner');
+banner.addEventListener('click', toggle);
+
+ const menu = document.querySelector('.menu');
+ menu.addEventListener('click', toggle);
+ menu.addEventListener('click', (e) => {
+  
+  e.stopPropagation();
+ });
+ ;
